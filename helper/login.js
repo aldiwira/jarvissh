dada9ff53ee77004166e69a98e8a4b4ffd620b72
aldi wirawan <aldiwira.aw@gmail.com>
2021-01-11 10:25:28 +0700

    fixing login flow

--- helper/login.js
+++ helper/login.js
@@ -16,18 +16,20 @@ module.exports = (bot) => {
     const password = msg.split(' ')[2];
 
     if (username && password) {
-      cruder.find(tableName.users, { username }).then((value) => {
+      cruder.find(tableName.users, { username }).then(async (value) => {
         if (password === value[0].password) {
-          updateLastLogin({ username }, ctx).then((val) => {
+          await updateLastLogin({ username }, ctx).then((val) => {
             if (val === 1) {
               ctx.reply(
                 `Selamat datang ${value[0].username} \nLast Login : ${value[0].last_login}`,
               );
+              ctx.scene.enter('home');
             }
           });
         } else {
-          ctx.reply('Password salah');
+          await ctx.reply('Password salah');
         }
+        await ctx.deleteMessage(ctx.message_id);
       });
     } else {
       ctx.reply(
@@ -36,11 +38,3 @@ module.exports = (bot) => {
     }
   });
 };
-
-// if (value[0].password) {
-//   ctx.reply(
-//     `Selamat datang ${username} \n Terakhir login : ${value[0].last_login}`,
-//   );
-// } else {
-//   ctx.reply('Password yang anda masukkan salah :(');
-// }
