178966f88624d02027d237ee6bc70dcaccff4486
aldiwira <aldiwira.aw@gmail.com>
2021-04-03 00:09:35 +0700

    add manage subscribe and blockcommand

--- stage/management.js
+++ stage/management.js
@@ -92,8 +92,80 @@ management.command('user', async (ctx) => {
   }
 });
 
-// TODO : Create Command for manage subscriber
-// TODO : Create Command for manage blacklist command
+// TODO : Create Command for manage subscriber (half Complate)
+management.command('pengikut', async (ctx) => {
+  const msg = ctx.update.message.text.split(' ');
+  const command = msg[1];
+  let replyMsg = ``;
+
+  if (command === 'list') {
+    replyMsg += `Daftar Pengikut Pesan Monitor Otomatis \n`;
+    replyMsg += `id \t username \n`;
+    replyMsg += `--------------------------\n`;
+    await cruder.read(tableName.subscriber).then((v) => {
+      v.map((val) => {
+        replyMsg += `${val.id}. \t ${val.username} \t \n`;
+      });
+    });
+    ctx.reply(replyMsg);
+  } else if (command === 'hapus') {
+    const id = msg[2];
+    if (id) {
+      const sub = await cruder.find(tableName.subscriber, { id });
+      const delSub = await knex(tableName.subscriber).where('id', id).del();
+      if (delSub === 1) {
+        await ctx.reply(`Pengikut ${sub[0].username} berhasil dihapus`);
+      } else {
+        await ctx.reply(`Pengikut tidak dapat ditemukan`);
+      }
+    } else {
+      await ctx.reply('Masukkan username yang akan dihapus');
+    }
+  }
+});
+// TODO : Create Command for manage blacklist command (Half Commplate)
+management.command('command', async (ctx) => {
+  const msg = ctx.update.message.text.split(' ');
+  const command = msg[1];
+  let replyMsg = ``;
+
+  if (command === 'list') {
+    replyMsg += `Daftar kata perintah terblokir \n`;
+    replyMsg += `id \t command \n`;
+    replyMsg += `--------------------------\n`;
+    await cruder.read(tableName.blacklist).then((v) => {
+      v.map((val) => {
+        replyMsg += `${val.id}. \t ${val.command} \t \n`;
+      });
+    });
+    ctx.reply(replyMsg);
+  } else if (command === 'delete') {
+    const id = msg[2];
+    if (id) {
+      const sub = await cruder.find(tableName.blacklist, { id });
+      const delSub = await knex(tableName.blacklist).where('id', id).del();
+      if (delSub === 1) {
+        await ctx.reply(`Kata Perintah ${sub[0].command} berhasil dihapus`);
+      } else {
+        await ctx.reply(`Kata Perintah tidak dapat ditemukan`);
+      }
+    } else {
+      await ctx.reply('Masukkan username yang akan dihapus');
+    }
+  } else if (command === 'add') {
+    const wordCommand = msg[2];
+    if (wordCommand) {
+      const commands = await cruder.find(tableName.blacklist, { wordCommand });
+      if (!commands.length) {
+        await cruder.insert(tableName.blacklist, { wordCommand }).then(() => {
+          ctx.reply(`Berhasil Memblokir kata perintah ${wordCommand}`);
+        });
+      }
+    }
+  }
+});
+
+// TODO : Add Subscribe this group with chat type state
 
 // leave command handler
 management.leave((ctx) => ctx.reply('Good bye'));
