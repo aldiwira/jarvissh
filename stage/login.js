0c39d6573c71cc868082df042a5e1289d501792a
aldiwira <aldiwira.aw@gmail.com>
2021-04-23 21:55:03 +0700

    Add register accses

--- stage/login.js
+++ stage/login.js
@@ -23,10 +23,34 @@ module.exports = (bot) => {
         ctx.session.users = checkAccount[0];
         ctx.scene.enter('home');
       } else {
-        ctx.reply(`${ctx.from.first_name}, ${msgtemp.processRegister}`);
+        ctx.reply(`Hai ${ctx.from.first_name}, ${msgtemp.processRegister}`);
       }
     } else {
-      ctx.reply(`${ctx.from.first_name}, ${msgtemp.notRegister}`);
+      ctx.reply(`Hai ${ctx.from.first_name}, ${msgtemp.notRegister}`);
+    }
+  });
+
+  // TODO : Registrasi account
+  bot.command('register', async (ctx) => {
+    const checkAccount = await checkAccounts({ telegram_id: ctx.from.id });
+    if (checkAccount.length === 0) {
+      const userData = {
+        username: ctx.from.first_name,
+        telegram_id: ctx.from.id,
+        isAdmin: 0,
+        isAllowed: 0,
+      };
+
+      await cruder.insert(tableName.users, userData).then((val) => {
+        ctx.reply(
+          `Selamat datang ${ctx.from.first_name}, ${msgtemp.successRegister}. ${val}`,
+        );
+      });
+    } else {
+      const stat = checkAccount[0].isAllowed
+        ? msgtemp.accountAllowed
+        : msgtemp.accountNotAllowed;
+      ctx.reply(`${ctx.from.first_name}, ${msgtemp.alreadyRegister}, ${stat}`);
     }
   });
 };
